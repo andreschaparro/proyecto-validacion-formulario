285e115bccfec628af2a42c8e0a4acab50cb71c3
andreschaparro <chaparroandres87@gmail.com>
2024-10-01 14:36:21 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Proyecto: Aplicación de validación de un formulario y uso de Firebase como Backend
+
+[LINK A LA WEB(NETLIFY)](https://proyecto-validacion-formulario.netlify.app/)
+
+Autor: Ing. Andres Chaparro
---
+++ script.js
@@ -0,0 +1,79 @@
+// Crear una cuenta en Firebase: https://firebase.google.com/
+// Crear un proyecto llamado 'validacion-formularios'
+// Crear una base de datos firestore en modo de prueba
+// Documentación: https://firebase.google.com/docs/firestore/quickstart
+
+// En la configuración del proyecto crear una aplicación web y copiar el firebaseConfig
+const firebaseConfig = {
+    apiKey: "AIzaSyAk8S1fD5ys1zyVE8W5T2MH4G2rmFWi32M",
+    authDomain: "validacion-formularios-80f9b.firebaseapp.com",
+    projectId: "validacion-formularios-80f9b",
+    storageBucket: "validacion-formularios-80f9b.appspot.com",
+    messagingSenderId: "758955969168",
+    appId: "1:758955969168:web:744fc868182f2051676615"
+}
+
+// La siguiente línea inicializa Firebase
+firebase.initializeApp(firebaseConfig)
+
+// La siguiente línea inicializa Firestore
+const db = firebase.firestore()
+
+document.getElementById('formulario').addEventListener('submit', (event) => {
+    // Evita el comportamiento por defecto de HTML para poder manejarlo desde JS
+    event.preventDefault()
+
+    // Las siguientes líneas validan el campo name
+    const entradaNombre = document.getElementById('name')
+    const divErrorNombre = document.getElementById('nameError')
+
+    if (entradaNombre.value.trim() === '') {
+        divErrorNombre.textContent = 'Por favor, introducí tu nombre'
+        divErrorNombre.classList.add('mensaje-error')
+    } else {
+        divErrorNombre.textContent = ''
+        divErrorNombre.classList.remove('mensaje-error')
+    }
+
+    // Las siguientes líneas validan el campo email utilizando una expresión regular
+    const entradaEmail = document.getElementById('email')
+    const divErrorEmail = document.getElementById('emailError')
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailPattern.test(entradaEmail.value)) {
+        divErrorEmail.textContent = 'Por favor, introducí un mail válido'
+        divErrorEmail.classList.add('mensaje-error')
+    } else {
+        divErrorEmail.textContent = ''
+        divErrorEmail.classList.remove('mensaje-error')
+    }
+
+    // Las siguientes líneas validan el campo password utilizando una expresión regular
+    const entradaPassword = document.getElementById('password')
+    const divErrorPassword = document.getElementById('passwordError')
+
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,15}$/
+    if (!passwordPattern.test(entradaPassword.value)) {
+        divErrorPassword.textContent = 'La contraseña debe tener al menos 8 caracteres, números, mayúsculas y minúsculas y caracteres especiales'
+        divErrorPassword.classList.add('mensaje-error')
+    } else {
+        divErrorPassword.textContent = ''
+        divErrorPassword.classList.remove('mensaje-error')
+    }
+
+    // Las siguientes líneas envían el formulario únicamente si los campos son válidos
+    if (!divErrorNombre.textContent && !divErrorEmail.textContent && !divErrorPassword.textContent) {
+        db.collection("users").add({
+            nombre: entradaNombre.value,
+            email: entradaEmail.value,
+            password: entradaPassword.value,
+        })
+            .then((docRef) => {
+                alert('El formulario se ha enviado con éxito', docRef.id)
+                document.getElementById('formulario').reset()
+            })
+            .catch((error) => {
+                alert(error)
+            })
+    }
+})
